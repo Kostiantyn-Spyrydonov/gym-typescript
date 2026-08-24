b9f6389496a547a267897f0de6d5779e1653fb28
Kostiantyn Spyrydonov <kostiantynspyrydonov@gmail.com>
2026-08-24 13:57:30 +0200

    feat: add Navbar(Header) component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import Navbar from "@/scenes/navbar/index"
+import { useEffect, useState } from "react"
+import { SelectedPage } from "@/shared/types"
+
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <div className='app'>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className='app bg-gray-20'>
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
